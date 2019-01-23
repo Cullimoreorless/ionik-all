dff6a7268afab851b4f47b299c8915f0c877d70d
Alex Cullimore <alex.m.cullimore@gmail.com>
2019-01-22 21:01:28 -0700

    updated to separate D3 from react

--- app/client/src/Graph.js
+++ app/client/src/Graph.js
@@ -25,6 +25,28 @@ class Graph extends Component{
   }
 
   
+
+  linkArc(d){
+    var dx = d.target.x - d.source.x,
+    dy = d.target.y - d.source.y,
+    dr = Math.sqrt(dx * dx + dy * dy),
+    plusdr = dr;
+    if(d.type === "OutsideOfWork"){
+      plusdr = dr + 100;
+    }
+    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + plusdr + " 0 0,1 " + d.target.x + "," + d.target.y;
+  }
+
+  transform(d){
+    return "translate(" + d.x + "," + d.y + ")";
+  }
+  
+  // sim;
+
+  // componentDidUpdate(){
+  //   this.getDataFromDB();
+  // }
+  
   getDataFromDB(){
     fetch("/molecule/getGraphData")
       .then(data => data.json())
@@ -37,13 +59,13 @@ class Graph extends Component{
         let preNodes = res.nodes;
         let nodes = {}
         let colorIndex = 0;
-        let colorCategory = {};
-        let colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+        var colorCategory = {};
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
     
         preNodes.forEach(node => {
           nodes[node.id] = node; 
-          if(colorCategory[node.location] == null){
-            colorCategory[node.location] = colorIndex++;
+          if(colorCategory[node.location || "nonegiven"] == null){
+            colorCategory[node.location || "nonegiven"] = colorIndex++;
             console.log(colorScale(colorIndex));
           }
         }); 
@@ -52,89 +74,98 @@ class Graph extends Component{
           link.source = nodes[link.source] || (nodes[link.source] = preNodes[link.source]);
           link.target = nodes[link.target] || (nodes[link.target] = preNodes[link.target]);    
         })
+
+        let sim = d3.forceSimulation(d3.values(nodes))
+          .force("link",
+            d3.forceLink().distance(200).links(links)
+          )
+          .force("x", d3.forceX(this.width / 2))
+          .force("y", d3.forceY(this.height / 2));
+
+        var path = svg.append("g").selectAll("path")
+          .data(links)
+          .enter().append("path")
+          .attr("class", function(d) { return "link " + d.type; })
+          .attr("style",function(d){return `stroke-width:${d.normalizedweight}px`})
+          .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
+        
+
+        var text = svg.append("g").selectAll("text")
+            .data(sim.nodes())
+          .enter().append("text")
+            .attr("x", 8)
+            .attr("y", ".35em")
+            .text(function(d) { return d.name; });  
+
+        // console.log(colorCategory);
+        // console.log(colorScale)
+        // console.log(d3.values(nodes));
+        let colors = {
+          "0":"#000",
+          "1":"#FFF",
+          "2":"#CCC",
+          "3":"#888"
+        }
+        function dragstarted(d) {
+          if (!d3.event.active) sim.alphaTarget(0.3).restart();
+          d.fx = d.x;
+          d.fy = d.y;
+        }
+      
+        function dragged(d) {
+          d.fx = d3.event.x;
+          d.fy = d3.event.y;
+        }
+      
+        function dragended(d) {
+          if (!d3.event.active) sim.alphaTarget(0);
+          d.fx = null;
+          d.fy = null;
+        } 
+        var circle = svg.append("g").selectAll("circle")
+             .data(sim.nodes())
+               .enter()
+            .append("circle")
+             .attr("r", function(d) { return d.normalizedweight;})
+             .style("fill",function(d) {
+               return colors[colorCategory[d.location ||"nonegiven"]]
+             })
+             .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended));
+            // .style("fill",function(d) { console.log(colorScale(colorCategory[d.location || "nonegiven"]));
+            //       return colorScale(colorCategory[d.location || "nonegiven"]);})
+            // .call(sim.drag);
+        console.log(circle);
         // Use elliptical arc path segments to doubly-encode directionality.
-        // let tick = ()=> {
-        //   path.attr("d", linkArc);
-        //   circle.attr("transform", transform);
-        //   text.attr("transform", transform);
-        // };
+        let tick = ()=> {
+          path.attr("d", this.linkArc);
+          circle.attr("transform", this.transform);
+          text.attr("transform", this.transform);
+          
+        };
 
-        // let linkArc =(d) => {
-        //   var dx = d.target.x - d.source.x,
-        //       dy = d.target.y - d.source.y,
-        //       dr = Math.sqrt(dx * dx + dy * dy),
-        //       plusdr = dr;
-        //   if(d.type === "OutsideOfWork"){
-        //     plusdr = dr + 100;
-        //   }
-        //   return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + plusdr + " 0 0,1 " + d.target.x + "," + d.target.y;
-        // };
+        sim.on('tick',tick);
 
-        // let transform = (d) => {
-        //   return "translate(" + d.x + "," + d.y + ")";
-        // };
-
-
-        // var sim = d3.forceSimulation(d3.values(nodes))
-        //   .force('link',d3.forceLink(links))
-        //   .force('center',d3.forceCenter())
-        //   ; 
+        
 
           
-      //   //sim.size([this.width, this.height])
-      //     // .linkDistance(200)
-      //     // .charge(-300)
-      //     sim.on("tick", tick)
-      //     // .start();
-        
-      //   svg.append("defs").selectAll("marker")
-      //     .data(["OutsideOfWork","RegularHours"])
-      //     .enter().append("marker")
-      //     .attr("id",function(d){ return d;})
-      //     .attr("viewBox","0 -5 10 10")
-      //     .attr("refX",15)
-      //     .attr("refY",-1.5)
-      //     .attr("markerWidth", 6)
-      //     .attr("markerHeight", 6)
-      //     .attr("orient", "auto")
-      //   .append("path")
-      //     .attr("d", "M0,-5L10,0L0,5");
-
-      //     var path = svg.append("g").selectAll("path")
-      //     .data(sim.links())
-      //   .enter().append("path")
-      //     .attr("class", function(d) { return "link " + d.type; })
-      //     .attr("style",function(d){return `stroke-width:${d.normalizedweight}px`})
-      //     .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
   
-      // var circle = svg.append("g").selectAll("circle")
-      //     .data(sim.nodes())
-      //   .enter().append("circle")
-      //     .attr("r", function(d) { return d.normalizedweight;})
-      //     .style("fill",function(d) { console.log(colorScale(colorCategory[d.location]));
-      //           return colorScale(colorCategory[d.location]);})
-      //     .call(sim.drag);
-  
-      // var text = svg.append("g").selectAll("text")
-      //     .data(sim.nodes())
-      //   .enter().append("text")
-      //     .attr("x", 8)
-      //     .attr("y", ".35em")
-      //     .text(function(d) { return d.name; });
   
        
-      this.force = d3.forceSimulation(d3.values(nodes))
-      .force("link",
-        d3.forceLink().distance(200).links(links)
-      )
-      .force("x", d3.forceX(this.width / 2))
-      .force("y", d3.forceY(this.height / 2));
+    //   this.force = d3.forceSimulation(d3.values(nodes))
+    //   .force("link",
+    //     d3.forceLink().distance(200).links(links)
+    //   )
+    //   .force("x", d3.forceX(this.width / 2))
+    //   .force("y", d3.forceY(this.height / 2));
 
-    this.force.on('tick', () => this.setState({
-      data:res,
-    	links:links,
-    	nodes: d3.values(nodes)
-    }));
+    // this.force.on('tick', () => this.setState({
+    //   data:res,
+    // 	links:links,
+    // 	nodes: d3.values(nodes)
+    // }));
         // this.setState({data:res, nodes:res.nodes, links:res.links}) 
       } )
   }
@@ -159,7 +190,7 @@ class Graph extends Component{
       <h1>New Component</h1>
       <svg ref={node => this.node = node}
       width={width} height={height}>
-        <g>
+        {/* <g>
           {links.map((link, index) => (
             
             <path className={"link " + link.type} 
@@ -182,7 +213,7 @@ class Graph extends Component{
               {node.name}
               </text>]
           ))}
-        </g>
+        </g> */}
       </svg>
     </div>
   }
