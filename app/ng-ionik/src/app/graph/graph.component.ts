ed16c65e02b615b42cdd3a96f196209e51c1df78
Alex Cullimore <alex.m.cullimore@gmail.com>
2019-02-09 17:15:46 -0700

    Need window resize service

--- app/ng-ionik/src/app/graph/graph.component.ts
+++ app/ng-ionik/src/app/graph/graph.component.ts
@@ -18,7 +18,8 @@ import * as d3 from 'd3';
   styleUrls: ['./graph.component.css']
 })
 export class GraphComponent implements OnInit {
-  @ViewChild("graphSVG") graphSVG : ElementRef;
+  @ViewChild('graphSVG') graphSVG : ElementRef;
+  @ViewChild('gContainer') graphContainer : ElementRef;
   nodes : Node[] =[]; 
   links : Link[] = [];
   graphData$: Observable<Object>;
@@ -71,84 +72,93 @@ export class GraphComponent implements OnInit {
       this.graphData = data;
       this.nodes = this.graphData.nodes;
       this.links = this.graphData.links;
-      console.log(this.nodes);
+      
       this.colorArray = this.nodes.reduce((acc, curr) =>{
         if(!(curr.location in acc)){
           acc.push(curr.location)
         }
         return acc;
       },[]);
-      let colorScale = this.getColorTransform();
-      let colorArrayLocal = this.colorArray;
-      let svg = d3.select(this.graphSVG.nativeElement)
-      svg.selectAll("*").remove();
-
-      this.links.forEach(link => {
-        link.source = this.nodes.find(n => n.id == link.source);
-        link.target = this.nodes.find(n => n.id == link.target);    
-      })
-
-      let sim = d3.forceSimulation(this.nodes)
-        .force("link",d3.forceLink().distance(200).links(this.links))
-        .force("charge", d3.forceManyBody())
-        .force("x", d3.forceX(this._options.width / 2))
-        .force("y", d3.forceY(this._options.height /2));
-
-      var path = svg.append("g").selectAll("path")
-        .data(this.links)
-        .enter().append("path")
-        .attr("class", function(d) { return "link " + d.type; })
-        .attr("style",function(d) {return `stroke-width:${d.normalizedweight}px`})
-        .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
       
-
-      var text = svg.append("g").selectAll("text")
-          .data(sim.nodes())
-        .enter().append("text")
-          .attr("x", function(d){return 3 + d.normalizedweight})
-          .attr("y", ".35em")
-          .text(function(d) { return d.name; });  
-
-      function dragstarted(d) {
-        if (!d3.event.active) sim.alphaTarget(0.3).restart();
-        d.fx = d.x;
-        d.fy = d.y;
-      }
-    
-      function dragged(d) {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
-      }
-    
-      function dragended(d) {
-        if (!d3.event.active) sim.alphaTarget(0);
-        d.fx = null;
-        d.fy = null;
-      }
-      var circle =
-        svg.append("g").selectAll("circle")
-          .data(sim.nodes())
-          .enter()
-            .append("circle")
-            .attr("r", function(d) { return d.normalizedweight;})
-            .style("fill",function(d){return colorScale(colorArrayLocal.findIndex(el => el == d.location))})
-            .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
-      
-      let tick = ()=> {
-        path.attr("d", this.linkArc);
-        circle.attr("transform", this.transform);
-        text.attr("transform", this.transform);
-        
-      };
-
-      sim.on('tick',tick);
-        
+      this.drawGraph();
       // this.graph = this.graphService.getForceGraph(this.nodes, this.links, this._options)
       // this.graph.initSimulation(this._options)
     })
+  }
+
+  resizeWindow(event){
+    console.log(this.graphContainer.nativeElement.width);
+  }
+
+  drawGraph(){
+    let colorScale = this.getColorTransform();
+    let colorArrayLocal = this.colorArray;
+    let svg = d3.select(this.graphSVG.nativeElement)
+    svg.selectAll('*').remove();
+
+    this.links.forEach(link => {
+      link.source = this.nodes.find(n => n.id == link.source);
+      link.target = this.nodes.find(n => n.id == link.target);    
+    })
+    
+    let sim = d3.forceSimulation(this.nodes)
+      .force('link',d3.forceLink().distance(200).links(this.links))
+      .force('charge', d3.forceManyBody())
+      .force('collision', d3.forceCollide().radius(function(d:any) {return d.normalizedweight + 10;}))
+      .force('x', d3.forceX(this._options.width / 2))
+      .force('y', d3.forceY(this._options.height /2));
+
+    var path = svg.append('g').selectAll('path')
+      .data(this.links)
+      .enter().append('path')
+      .attr('class', function(d) { return 'link ' + d.type; })
+      .attr('style',function(d) {return `stroke-width:${d.normalizedweight}px`})
+      .attr('marker-end', function(d) { return 'url(#' + d.type + ')'; });
+    
+
+    var text = svg.append('g').selectAll('text')
+        .data(sim.nodes())
+      .enter().append('text')
+        .attr('x', function(d){return 3 + d.normalizedweight})
+        .attr('y', '.35em')
+        .text(function(d) { return d.name; });  
+
+    function dragstarted(d) {
+      if (!d3.event.active) sim.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+  
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+  
+    function dragended(d) {
+      if (!d3.event.active) sim.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+    var circle =
+      svg.append('g').selectAll('circle')
+        .data(sim.nodes())
+        .enter()
+          .append('circle')
+          .attr('r', function(d) { return d.normalizedweight;})
+          .style('fill',function(d){return colorScale(colorArrayLocal.findIndex(el => el == d.location))})
+          .call(d3.drag()
+          .on('start', dragstarted)
+          .on('drag', dragged)
+          .on('end', dragended));
+    
+    let tick = ()=> {
+      path.attr('d', this.linkArc);
+      circle.attr('transform', this.transform);
+      text.attr('transform', this.transform);
+      
+    };
+
+    sim.on('tick',tick);
   }
 
   linkArc(d){
@@ -156,14 +166,14 @@ export class GraphComponent implements OnInit {
     dy = d.target.y - d.source.y,
     dr = Math.sqrt(dx * dx + dy * dy),
     plusdr = dr;
-    if(d.type === "OutsideOfWork"){
+    if(d.type === 'OutsideOfWork'){
       plusdr = dr + 100;
     }
-    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + plusdr + " 0 0,1 " + d.target.x + "," + d.target.y;
+    return 'M' + d.source.x + ',' + d.source.y + 'A' + dr + ',' + plusdr + ' 0 0,1 ' + d.target.x + ',' + d.target.y;
   }
 
   transform(d){
-    return "translate(" + d.x + "," + d.y + ")";
+    return 'translate(' + d.x + ',' + d.y + ')';
   }
 
   getColorTransform(){
